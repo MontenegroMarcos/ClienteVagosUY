9889f9c2a53679484d15a15fe13dda369a31f232
MontenegroMarcos <mmontenegro@correo.um.edu.uy>
2022-10-04 13:53:05 -0300

    Se agrega modificar y seleccionar, sin prueba

--- src/main/java/com/Olimpia/demo/UI/UsuarioAdminController.java
+++ src/main/java/com/Olimpia/demo/UI/UsuarioAdminController.java
@@ -2,20 +2,25 @@ package com.Olimpia.demo.UI;
 
 import com.Olimpia.demo.ModeloEmpresa;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.scene.control.cell.PropertyValueFactory;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 
-import java.io.File;
+import java.awt.event.MouseEvent;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class UsuarioAdminController {
+public class UsuarioAdminController implements Initializable {
 
     @FXML
     private TextField nombreEmpresa;
@@ -40,6 +45,12 @@ public class UsuarioAdminController {
 
     @FXML
     private TableColumn colPSWEmpresa;
+
+    private ObservableList<ModeloEmpresa> empresasLista;
+
+
+
+
 
 
     @FXML
@@ -81,7 +92,7 @@ public class UsuarioAdminController {
     }
 
     @FXML
-    protected void obtenerEmpresas(){
+    protected ObservableList obtenerEmpresas(){
         ObjectMapper mapper = new ObjectMapper();
         String empresas = Unirest.get("http://10.252.60.160:8080/vagouy/empresa").asString().getBody();
         try {
@@ -90,6 +101,7 @@ public class UsuarioAdminController {
         }catch(Exception e){
             e.printStackTrace();
         }
+        return null;
     }
 
 
@@ -98,6 +110,72 @@ public class UsuarioAdminController {
         nombreEmpresa.setText(null);
         emailEmpresa.setText(null);
         contraseniaEmpresa.setText(null);
+
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        empresasLista = FXCollections.observableArrayList();
+        this.colnombreEmpresa.setCellFactory(new PropertyValueFactory("nombre"));
+        this.colEmailEmpresa.setCellFactory(new PropertyValueFactory("email"));
+        this.colPSWEmpresa.setCellFactory(new PropertyValueFactory("psw"));
+    }
+
+    @FXML
+    public void actualizarTblEmpresas(){
+        this.tablaEmpresa.setItems(obtenerEmpresas());
+
+    }
+
+    @FXML
+    private void seleccionarEmpresa(MouseEvent event){
+
+        ModeloEmpresa empresa = (ModeloEmpresa) this.tablaEmpresa.getSelectionModel().getSelectedItem();
+
+        if(empresa.equals(null)){
+
+        } else {
+            this.nombreEmpresa.setText(empresa.getNombre());
+            this.emailEmpresa.setText(empresa.getEmail());
+            this.contraseniaEmpresa.setText(empresa.getPsw());
+        }
+    }
+
+
+    @FXML
+    private void modificarEmpresa(){
+        ModeloEmpresa empresa = (ModeloEmpresa) this.tablaEmpresa.getSelectionModel().getSelectedItem();
+
+        if(empresa.equals(null)){
+
+        } else {
+
+            try {
+                String nombreCliente = nombreEmpresa.getText();
+                String email = emailEmpresa.getText();
+                String password = contraseniaEmpresa.getText();
+
+
+                ModeloEmpresa empresaAux = new ModeloEmpresa(email,nombreCliente,password);
+
+                if(!this.empresasLista.contains(empresaAux)){
+                    empresa.setNombre(empresaAux.getNombre());
+                    empresa.setEmail(empresaAux.getEmail());
+                    empresa.setPsw(empresaAux.getPsw());
+
+
+                    this.tablaEmpresa.refresh();
+
+
+                }
+
+
+            }catch (Exception e){
+
+            }
+        }
 
 
     }
