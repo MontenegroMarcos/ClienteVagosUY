de39563ec287d1480288ec7ee3cb81b3908332c6
marcos <mmontenegro@gmail.com>
2022-10-11 12:47:32 -0300

    se crea el metodo de validar login

--- src/main/java/com/Olimpia/demo/UI/InicioController.java
+++ src/main/java/com/Olimpia/demo/UI/InicioController.java
@@ -7,17 +7,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+ import javafx.scene.control.Label;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InicioController implements Initializable {
-
-
     private Stage estage;
+
+    @FXML
+    private Label alertLabel;
 
     @FXML
     private Button CentroDeportivobtn;
@@ -39,27 +42,14 @@ public class InicioController implements Initializable {
 
     @FXML
     private Label verdaderapassword;
-
-
     @FXML
     void entrarUsuarioCentroDeportivo() throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent root = fxmlLoader.load(InicioController.class.getResourceAsStream("CentroDeportivoUI.fxml"));
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
-
-
-        /*FXMLLoader fxmlLoader = new FXMLLoader(InicioController.class.getResource("CentroDeportivoUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 400);
-        Stage stage = null;
-        stage.setTitle("GymAPP");
-        stage.setScene(scene);
-        stage.show();*/
     }
-
-
     @FXML
     void entrarUsuarioAdmin() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
@@ -69,48 +59,55 @@ public class InicioController implements Initializable {
         stage.setScene(scene);
         stage.show();
         this.estage.close();
-
     }
-
     public void setStage(Stage stage) {
         estage = stage;
+        //guarda info pantalla
     }
-
-
-
-   @FXML
+    @FXML
    public void mostrarContaseña(ActionEvent event){
-
         if(mostrarOcultar.isSelected()){
             verdaderapassword.setVisible(true);
             verdaderapassword.setText(campoPassword.getText());
             mostrarOcultar.setText("Ocultar");
         } else {
-
             verdaderapassword.setVisible(false);
             mostrarOcultar.setText("Mostrar");
         }
-
    }
-
-
-    @Override
+   @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+    public void mostrarContasenia(javafx.event.ActionEvent actionEvent) {
+    }
+    @FXML
+    public void entrarUsuariofinalLogin(){
 
+        String correo = campoEmail.getText();
+        String psw = campoPassword.getText();
+
+        if(validarlogin(correo,psw)){
+            //Desarollo
+        } else {
+            alertLabel.setText("Error, correo y/o contraseña incorrectas");
+        }
+
+    }
+    public boolean validarlogin(String email, String psw){
+        //Aqui Santiago obtiene el get de la base de datos
+        boolean result = false;
+
+
+
+        return result;
 
     }
 
 
-    /*@FXML
-    void entrarUsuarioAdmin() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent root = fxmlLoader.load(InicioController.class.getResourceAsStream("UsuarioAdminUI.fxml"));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root));
-        stage.show();
 
-    }*/
+
+
 
 
 }
