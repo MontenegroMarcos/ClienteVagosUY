365e6682cb9fc50fd3420b2ca72f2c2c0c722f04
MontenegroMarcos <marcosmontenegro434@gmail.com>
2022-10-31 15:47:40 -0300

    se crean operaciones de Centro Deportivo

--- src/main/java/com/Olimpia/demo/UI/CentroDeportivoController.java
+++ src/main/java/com/Olimpia/demo/UI/CentroDeportivoController.java
@@ -1,15 +1,21 @@
 package com.Olimpia.demo.UI;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import kong.unirest.Unirest;
 
-public class CentroDeportivoController {
+import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class CentroDeportivoController implements Initializable {
 
     @FXML
     private Button AgregarActividad;
@@ -34,6 +40,9 @@ public class CentroDeportivoController {
 
     @FXML
     private TextField checkinCorreo;
+
+    @FXML
+    private ComboBox<String> checkinAcrividades;
 
     @FXML
     private ComboBox<?> comboboxCategoriasAgregar;
@@ -74,8 +83,74 @@ public class CentroDeportivoController {
     @FXML
     private Text textoUsuario;
 
+    @FXML
+    private ComboBox<Integer> horarioInicio;
+
+    @FXML
+    private DatePicker selectorFecha;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private List<List> obtenerActividades() {
+        ObjectMapper mapper = new ObjectMapper();
+        String actividades = Unirest.get("http://localhost:8080/vagouy/Actividades/Todas").asString().getBody();
+        List<List> actividad = null;
+        try {
+            actividad = mapper.readValue(actividades, new TypeReference<List<List>>() {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return actividad;
+    }
+
+    public List<List> obtenerActividadesPorNombre(String nombre) {
+        ObjectMapper mapper = new ObjectMapper();
+        String actividades = Unirest.get("http://localhost:8080/vagouy/Actividades/" + nombre).asString().getBody();
+        List<List> actividad = null;
+        try {
+            actividad = mapper.readValue(actividades, new TypeReference<List<List>>() {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return actividad;
+    }
+
 
     //Check-In
+
+    @FXML
+    private void Checkin(){
+
+        String nombreUser = nombreCheckin.getText();
+        String actividad = checkinAcrividades.getValue();
+        DayOfWeek fecha = selectorFecha.getValue().getDayOfWeek();
+        int horaInicio = horarioInicio.getValue();
+
+
+        if(confirmarReserva(nombreUser,actividad)){
+
+        } else {
+            //Manda Error
+        }
+    }
+    @FXML
+    private void limpiarCheckin(){
+
+        this.nombreCheckin.setText(null);
+
+
+
+    }
+    private boolean confirmarReserva(String nombre, String Actividad){
+
+        return false;
+    }
 
     //Actividades
 
@@ -83,10 +158,11 @@ public class CentroDeportivoController {
 
     //Balance
 
+    @FXML
+    void obtenerBalance(){
 
-
-
-
+        String Currency = "$";
+    }
 
 
 
